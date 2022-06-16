359c7610962f57c26311136337d4cb5368c6a823
ngngocanh1007 <nguyenngocanh100720@gmail.com>
2022-06-16 15:04:59 +0700

    may-tinh

---
+++ action.js
@@ -0,0 +1,12 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+    
+function display(value) {
+    document.getElementById("result").value += value;
+}
+function main() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
